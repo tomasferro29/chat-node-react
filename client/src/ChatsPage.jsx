15acaae71d2efc33e2b7c5a065239b2e397995fc
Tomas Ferro <tomasferro29@gmail.com>
2023-08-28 11:32:56 -0300

    add chat bewtween users

--- client/src/ChatsPage.jsx
+++ client/src/ChatsPage.jsx
@@ -1,7 +1,17 @@
-const ChatsPage = () => {
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    
+    const chatProps = useMultiChatLogic(
+        'c29e8e07-a0b9-4fb8-b798-277872379d62',
+        props.user.username,
+        props.user.secret
+    )
+
     return (
-        <div>
-            ...chats
+        <div style={{ height :'100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
         </div>
     )
 }
