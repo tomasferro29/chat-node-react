a412ed0496403b310c714b1ff6635a0ce879013f
Tomas Ferro <tomasferro29@gmail.com>
2023-08-28 10:30:05 -0300

    add register for new user

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require("axios");
+const port = 3001
+const app = express();
+app.use(express.json());
+app.use(cors({origin: true}));
+
+app.post('/authenticate', async (req, res) => {
+    const {username} = req.body;
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username : username, secret: username, first_name: username },
+            { headers : { "private-key": "bfb168b2-2342-42a0-a6b3-b6bf49a5ba50" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (error) {
+        console.log(error.message)
+    }
+    return res.json({username: username, secret: "sha256..."});
+});
+
+
+app.listen(port, () => {
+    console.log(`server listening port ${port}`)
+});
